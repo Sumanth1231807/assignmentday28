ffbb80d3a6fe3462ce4544d0ae204d6782531f35
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-28 20:11:29 +0530

    area pytest

---
+++ test_area.py
@@ -0,0 +1,33 @@
+import calculation
+import pytest
+
+def test_area_square():
+    side = 3
+    result = calculation.area_square(side)
+    assert result == side * side
+
+def test_perimeter_rect():
+    l=2
+    w=3
+    result=calculation.perimeter_rect(l,w)
+    assert result == 2*(l+w)
+
+def test_area_rect():
+    l=3
+    b=2
+    result=calculation.area_rect(l,b)
+    assert result == l*b
+
+def test_area_circle():
+    r=4
+    PI = 3.14
+    result=calculation.area_circle(r)
+    assert result == PI * r * r
+
+def test_area_triangle():
+    a=3
+    b=4
+    c=5
+    s=(a+b+c)/2
+    result=calculation.area_triangle(a,b,c)
+    assert result == (s * (s - a) * (s - b) * (s - c)) ** 0.5
